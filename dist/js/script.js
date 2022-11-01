7f52addb6dc127f43add19dcbd0b07c7a59d5f20
rikarani <phasyaananta1502@gmail.com>
2022-11-01 23:45:30 +0700

    bikin event pas tombol generate user diklik

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,7 @@
 // * Import Function
 import { generateUser } from "../../src/js/fetch.js";
 
-window.addEventListener("load", generateUser());
+window.addEventListener("load", generateUser);
+
+const newProfileButton = document.getElementById("new-profile");
+newProfileButton.addEventListener("click", generateUser);
