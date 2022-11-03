da9d0f006953ec8f91b5d3957357fbf6cdfc4ebf
rikarani <phasyaananta1502@gmail.com>
2022-11-04 00:29:25 +0700

    bikin event pas tombol copy di-klik, bikin efek stabilo

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,25 @@
 // * Import Function
 import { generateUser } from "../../src/js/fetch.js";
 
+// Langsung generate profile pas Halaman pertama kali dibuka
 window.addEventListener("load", generateUser);
 
+// Generate Profile Baru
 const newProfileButton = document.getElementById("new-profile");
 newProfileButton.addEventListener("click", generateUser);
+
+// Salin Profile
+const copyProfileButton = document.getElementById("copy-profile");
+copyProfileButton.addEventListener("click", () => {
+  const nama = document.getElementById("nama");
+
+  // Bikin Efek Stabilo
+  const selection = window.getSelection();
+  const range = document.createRange();
+  range.selectNode(nama);
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  // Copy to Clipboard
+  navigator.clipboard.writeText(nama.textContent);
+});
